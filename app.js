d6e924aa2629d2e5ed185fb6929092489b44a371
nori <norinorimcz@gmail.com>
2020-04-14 09:53:49 +0900

    最初のコミット。環境構築まで

---
+++ app.js
@@ -0,0 +1,48 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+// let indexRouter = require('./routes/index');
+// let usersRouter = require('./client/src/index');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.get('*', (req, res) => {
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname+'/client/build/index.html'))
+})
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const port = process.env.PORT || 3001
+app.listen(port, () => console.log(`Server running on port ${port}`))
+
+module.exports = app;
